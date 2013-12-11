acaf3216c9f3780ca4e88fdb3ec2e35fbe239f57
wasja1982 <wasja@yandex.ru>
2013-12-11 09:32:15 +0300

    Подкорректирована работа в случае, если загрузить на сервер не удалось. Более корректное переопределение метода SaveFile.

--- classes/modules/image/Image.class.php
+++ classes/modules/image/Image.class.php
@@ -15,13 +15,10 @@ class PluginSelectelstorage_ModuleImage extends PluginSelectelstorage_Inherit_Mo
      * Сохранение в Selectel Storage
      */
     public function UploadToSelectelstorage($sFilePath) {
-
         if (extension_loaded('curl')) {
-
             $sS = new SelectelStorage(Config::Get('plugin.selectelstorage.user'), Config::Get('plugin.selectelstorage.password'));
 
             try {
-
                 $bucket = $sS->getContainer(Config::Get('plugin.selectelstorage.bucket'));
 
                 $sName = explode(trim(Config::Get('path.uploads.root')).'/', $sFilePath);
@@ -34,15 +31,14 @@ class PluginSelectelstorage_ModuleImage extends PluginSelectelstorage_Inherit_Mo
                 }
                 else {
                     error_log('Something wrong while uploading file to selectel storage');
-                    return false;
+                    return $sFilePath;
                 }
             }
             catch(Exception $e) {
                 error_log("Something wrong while uploading file to selectel storage. Error: ".$e->getMessage());
-                return false;
+                return $sFilePath;
             }
         }
-
         return false;
     }
 
@@ -50,27 +46,11 @@ class PluginSelectelstorage_ModuleImage extends PluginSelectelstorage_Inherit_Mo
      * Изменяем логику сохранения файла
      */
     public function SaveFile($sFileSource,$sDirDest,$sFileDest,$iMode=null,$bRemoveSource=false) {
-        $sFileDestFullPath=rtrim(Config::Get('path.root.server'),"/").'/'.trim($sDirDest,"/").'/'.$sFileDest;
-        $this->CreateDirectory($sDirDest);
-
-        $bResult=copy($sFileSource,$sFileDestFullPath);
-        if ($bResult and !is_null($iMode)) {
-            chmod($sFileDestFullPath,$iMode);
-        }
-        if(!strpos($sDirDest,'/tmp/avatars/') && !strpos($sDirDest,'/tmp/fotos/')) {
+        $sFileDestFullPath = parent::SaveFile($sFileSource,$sDirDest,$sFileDest,$iMode,$bRemoveSource);
+        if ($sFileDestFullPath && !strpos($sDirDest,'/tmp/avatars/') && !strpos($sDirDest,'/tmp/fotos/')) {
             $sFileDestFullPath = $this->UploadToSelectelStorage($sFileDestFullPath);
         }
-
-        if ($bRemoveSource) {
-            @unlink($sFileSource);
-        }
-        /**
-         * Если копирование прошло успешно, возвращаем новый серверный путь до файла
-         */
-        if ($bResult) {
-            return $sFileDestFullPath;
-        }
-        return false;
+        return $sFileDestFullPath;
     }
 
 }
